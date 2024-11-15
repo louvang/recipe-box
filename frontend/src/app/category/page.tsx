84dcefc7814aef35fde546f7c6f27bcebe9eb0aa
Lou Vang <louvangdev@gmail.com>
2024-11-14 21:11:52 -0600

    Move category fetch logic into utils file

--- frontend/src/app/category/page.tsx
+++ frontend/src/app/category/page.tsx
@@ -1,15 +1,5 @@
 import Link from 'next/link';
-
-async function fetchCategories() {
-  const res = await fetch(`${process.env.STRAPI_URL}/api/categories`, {
-    cache: 'force-cache', // Data is fetched only once at build time
-  });
-  if (!res.ok) {
-    throw new Error('Failed to fetch categories');
-  }
-  const data = await res.json();
-  return data.data;
-}
+import { fetchAllCategories } from '@/utils/categoryService';
 
 export interface ICategory {
   id: number;
@@ -23,7 +13,7 @@ export interface ICategory {
 }
 
 export default async function Categories() {
-  const categories = await fetchCategories();
+  const categories = await fetchAllCategories();
 
   const liClass =
     'border my-2 px-4 py-2 rounded-md hover:bg-slate-100 hover:cursor-pointer';
--- frontend/src/utils/categoryService.ts
+++ frontend/src/utils/categoryService.ts
@@ -1,16 +1,20 @@
-export async function fetchAllCategries() {
-  const res = await fetch(`${process.env.STRAPI_URL}/api/categories`);
+export async function fetchAllCategories() {
+  const res = await fetch(`${process.env.STRAPI_URL}/api/categories`, {
+    cache: 'force-cache', // Data is fetched only once at build time
+  });
   if (!res.ok) {
     throw new Error('Failed to fetch all categories');
   }
   const data = await res.json();
-  return data;
+  return data.data;
 }
 
-export async function fetchSingleCategory(id: string) {
-  const res = await fetch(`${process.env.STRAPI_URL}/api/categories/${id}`);
+export async function fetchSingleCategory(slug: string) {
+  const res = await fetch(
+    `${process.env.STRAPI_URL}/api/categories?[slug][$eq]=${slug}`
+  );
   if (!res.ok) {
-    throw new Error(`Failed to fetch category with id '${id}'`);
+    throw new Error(`Failed to fetch category with slug '${slug}'`);
   }
   const data = await res.json();
   return data;
