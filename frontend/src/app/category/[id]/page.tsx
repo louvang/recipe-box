0dc65e84f3b8c5c143699db8bc755b7514a58743
louvang <vanglou@proton.me>
2025-02-03 18:31:05 -0600

    Add view category by id pages

--- frontend/src/app/category/[id]/page.tsx
+++ frontend/src/app/category/[id]/page.tsx
@@ -1,3 +1,11 @@
+"use client";
+
+import { useRouter } from 'next/navigation';
+
 export default function Category() {
-  return <div>Display list of recipes in category of &quot;id&quot;</div>;
+  const router = useRouter();
+
+  console.log(router)
+
+  return <div>Display list of recipes in category of &quot;id&quot;! You are in the category: </div>;
 }
--- frontend/src/app/category/page.tsx
+++ frontend/src/app/category/page.tsx
@@ -28,6 +28,5 @@ export default async function Categories() {
           </li>
         ))}
       </ul>
-    </div>
-  );
+    </div>  );
 }
