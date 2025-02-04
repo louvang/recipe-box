6b9c5b12acbad08d61daa7e06b2832db4d8a8413
louvang <vanglou@proton.me>
2025-02-03 22:02:38 -0600

    Add configuration for AWS media uploads

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -1,7 +1,20 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', process.env.CDN_URL],
+          'media-src': ["'self'", 'data:', 'blob:', process.env.CDN_URL],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
